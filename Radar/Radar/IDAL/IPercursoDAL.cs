3bf1956b22e98986cc37dd5e099deb0a52f62c33
landim32 <rodrigo@cmapps.com.br>
2016-10-26 17:27:13 -0200

    Inclusão de Percurso

---
+++ Radar/Radar/IDAL/IPercursoDAL.cs
@@ -0,0 +1,17 @@
+﻿using Radar.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Radar.IDAL
+{
+    public interface IPercursoDAL
+    {
+        IList<PercursoInfo> listar();
+        PercursoInfo pegar(int idPercurso);
+        int gravar(PercursoInfo percurso);
+        void excluir(int id);
+    }
+}
---
+++ Radar/Radar/Model/PercursoPontoInfo.cs
@@ -0,0 +1,20 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Radar.Model
+{
+    [Table("percurso_ponto")]
+    public class PercursoPontoInfo
+    {
+        [AutoIncrement, PrimaryKey]
+        public int Id { get; set; }
+        public int IdPercurso { get; set; }
+        public DateTime Data { get; set; }
+        public double Velocidade { get; set; }
+        public bool Movimento { get; set; }
+    }
+}
