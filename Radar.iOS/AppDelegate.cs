880df15257f4bb96f2142ad1afad6dd233352c2d
landim32 <rodrigo@cmapps.com.br>
2016-10-31 14:40:16 -0200

    .

--- Radar.iOS/AppDelegate.cs
+++ Radar.iOS/AppDelegate.cs
@@ -21,8 +21,8 @@ namespace Radar.iOS
 
             bool retorno = base.FinishedLaunching(app, options);
 
-            GPSiOS gps = new GPSiOS();
-            gps.inicializar();
+            //GPSiOS gps = new GPSiOS();
+            //gps.inicializar();
 
             return retorno;
         }
