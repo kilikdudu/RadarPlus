2b2f108f23d3b80fea6b9635105cfd2a72dedcfe
landim32 <rodrigo@cmapps.com.br>
2016-11-01 20:50:46 -0200

    Mapa IOS

---
+++ Radar.iOS/RadarMKAnnotation.cs
@@ -0,0 +1,27 @@
+﻿using MapKit;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using CoreLocation;
+using Radar.Controls;
+
+namespace Radar.iOS
+{
+    public class RadarMKAnnotation : MKAnnotation
+    {
+        public RadarPin Radar { get; set; }
+
+        public override CLLocationCoordinate2D Coordinate {
+            get {
+                if (Radar != null) {
+                    return new CLLocationCoordinate2D(Radar.Pin.Position.Latitude, Radar.Pin.Position.Longitude);
+                }
+                return new CLLocationCoordinate2D();
+            }
+        }
+
+        public RadarMKAnnotation(RadarPin radar) {
+            Radar = radar;
+        }
+    }
+}
