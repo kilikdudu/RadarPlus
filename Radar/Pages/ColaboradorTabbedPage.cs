9914f31814d56f409f318f10299fb34ae5072ae4
landim32 <rodrigo@cmapps.com.br>
2016-12-21 11:24:57 -0200

    Organização de arquivos

---
+++ Radar/Pages/ColaboradorTabbedPage.cs
@@ -0,0 +1,18 @@
+﻿using Xamarin.Forms;
+
+namespace Radar.Pages
+{
+	public class ColaboradorTabbedPage : TabbedPage
+	{
+		public ColaboradorTabbedPage()
+		{
+			var navigationPage = new NavigationPage(new ColaboradorPage());
+			//navigationPage.Icon = "schedule.png";
+			//navigationPage.Title = "Schedule";
+
+			Children.Add(new ColaboradorAdministracaoPage());
+			Children.Add(navigationPage);
+
+		}
+	}
+}
